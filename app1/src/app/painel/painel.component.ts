943e004c17cbb1e1c38731c60d8cbf6cb63194ad
Rodrigo Garcia Buzato <rodrigogbuzato@hotmail.com>
2020-01-02 15:37:40 -0300

    verificando a resposta

--- app1/src/app/painel/painel.component.ts
+++ app1/src/app/painel/painel.component.ts
@@ -12,10 +12,41 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase:'
+  public resposta: string;
 
-  constructor() { }
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log(this.rodadaFrase);   
+  }
 
   ngOnInit() {
+  }
+
+  public atualizaResposta(resposta: Event): void {
+
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+    console.log(this.resposta);
+
+  }
+
+  public verificarResposta(): void {
+
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+ 
+      alert('A tradução está correta');
+
+      this.rodada++
+      this.rodadaFrase = this.frases[this.rodada];
+
+    } else {
+
+      alert('A tradução está errada');
+    
+    }
+  
   }
 
 }
