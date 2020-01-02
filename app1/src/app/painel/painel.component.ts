d1e8d577a944be61e81dab2243685270c6de98c0
Rodrigo Garcia Buzato <rodrigogbuzato@hotmail.com>
2020-01-02 17:10:46 -0300

    Alteracao para limpar o campo de texto

--- app1/src/app/painel/painel.component.ts
+++ app1/src/app/painel/painel.component.ts
@@ -12,14 +12,16 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase:'
-  public resposta: string;
+  public resposta: string = '';
 
   public rodada: number = 0;
   public rodadaFrase: Frase;
 
+  public progresso: number = 0;
+
   constructor() {
-    this.rodadaFrase = this.frases[this.rodada];
-    console.log(this.rodadaFrase);   
+    this.atualizaRodada();
+
   }
 
   ngOnInit() {
@@ -37,9 +39,9 @@ export class PainelComponent implements OnInit {
     if (this.rodadaFrase.frasePtBr == this.resposta) {
  
       alert('A tradução está correta');
-
-      this.rodada++
-      this.rodadaFrase = this.frases[this.rodada];
+      this.rodada++;
+      this.progresso = this.progresso + (100 / this.frases.length);
+      this.atualizaRodada();
 
     } else {
 
@@ -48,5 +50,10 @@ export class PainelComponent implements OnInit {
     }
   
   }
+
+  public atualizaRodada(): void {
+    this.rodadaFrase = this.frases[this.rodada];
+    this.resposta = '';
+  } 
 
 }
--- app1/src/app/progresso/progresso.component.ts
+++ app1/src/app/progresso/progresso.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-progresso',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./progresso.component.css']
 })
 export class ProgressoComponent implements OnInit {
+
+  // Input() define que a variável progresso pode receber parametros externos
+  // (vindo de outro componente)
+  @Input() public progresso: number = 0;
+
 
   constructor() { }
 
