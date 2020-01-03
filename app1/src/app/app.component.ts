1d05fa28ee498705e97c06817ce77b0598407814
Rodrigo Garcia Buzato <rodrigogbuzato@hotmail.com>
2020-01-03 14:45:45 -0300

    finalizacao do app

--- app1/src/app/app.component.ts
+++ app1/src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app1';
+
+  public jogoEmAndamento: boolean = true;
+  public tipoEncerramento: string;
+  
+  public encerrarJogo(tipo: string): void {
+    console.log(tipo);
+    this.tipoEncerramento = tipo;
+    this.jogoEmAndamento = false;
+  }
+
+  public reiniciarJogo(): void {
+    this.jogoEmAndamento = true;
+    this.tipoEncerramento = undefined;
+  }
+
 }
--- app1/src/app/painel/painel.component.ts
+++ app1/src/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
 
 import { Frase } from '../shared/frase.model'
 import { FRASES } from './frases-mock'
@@ -21,12 +21,18 @@ export class PainelComponent implements OnInit {
 
   public tentativas: number = 3;
 
+  @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter();
+
   constructor() {
     this.atualizaRodada();
 
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    console.log('O componente Painel foi destruído');
   }
 
   public atualizaResposta(resposta: Event): void {
@@ -39,19 +45,22 @@ export class PainelComponent implements OnInit {
   public verificarResposta(): void {
     console.log(this.tentativas);
     if (this.rodadaFrase.frasePtBr == this.resposta) {
- 
-      alert('A tradução está correta');
+
       this.rodada++;
       this.progresso = this.progresso + (100 / this.frases.length);
+
+      if(this.rodada === 4) {
+        this.encerrarJogo.emit('vitoria');
+      }
+
       this.atualizaRodada();
 
     } else {
 
-      alert('A tradução está errada');
       this.tentativas--;
 
       if(this.tentativas === -1) {
-        alert('Você perdeu todas as tentativas');
+        this.encerrarJogo.emit('derrota');
       }
     
     }
